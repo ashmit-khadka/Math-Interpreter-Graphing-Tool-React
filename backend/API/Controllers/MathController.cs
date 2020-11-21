ab7a20512778c995d932f9f0633ed96072210195
GeorgeWebster1998 <72751923+GeorgeWebster1998@users.noreply.github.com>
2020-11-21 12:28:05 +0000

    Updated Math Controller

--- backend/API/Controllers/MathController.cs
+++ backend/API/Controllers/MathController.cs
@@ -11,7 +11,6 @@ namespace API.Controllers
     public class MathController : ApiController
     {
         // GET: api/Math
-        /*
         public MathResponse Get(string equation)
         {
 
@@ -21,19 +20,27 @@ namespace API.Controllers
             };
             return response;
         }
-        */
 
-        public MathResponse Get(string problem)
+        public MathResponse Test(string equation)
         {
+            int MAX_TOKENS = 16; //Maximum number of tokens the lexer will define
+            int MAX_CHAR = 16; //Maximum characters taken from input
+            LookupTable lt = new LookupTable(MAX_TOKENS); // Class to store Tokens and Symbols
+            int NO_tokens; //Number of tokens
+            char[] input = equation.ToCharArray();
+            Lexer lex = new Lexer(ref input, ref MAX_TOKENS, ref lt);
+            lex.Process();   
+            Parser parser = new Parser(ref lt);
+            parser.Parse();
+            Executor executor = new Executor(ref lt);
+            double result = executor.ShuntYard();
 
             var response = new MathResponse
             {
-                Result = Interpreter.Interpreter.Calc(problem)
+                Result = result
             };
-
             return response;
         }
-
 
         // GET: api/Math/5
         public string Get(int id)
